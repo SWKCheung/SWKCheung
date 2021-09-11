84157e707758aec5a90634fd86befc165facc4e6
SWKCheung <samsui@hotmail.com>
2021-09-11 12:24:32 -0400

    deployment of ocmapp

---
+++ reactjs/build/precache-manifest.76e72bd913bf8549201c6e1493c472fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69d594aa6de5c2fd4018dd4459cc3c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbb7b303c0a868c7cdf8",
+    "url": "/static/css/main.8618fcca.chunk.css"
+  },
+  {
+    "revision": "cbbd7d90aff0959a744f",
+    "url": "/static/js/2.ed8e6069.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ed8e6069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb7b303c0a868c7cdf8",
+    "url": "/static/js/main.060aa135.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
